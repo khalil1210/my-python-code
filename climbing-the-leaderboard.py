3d77ff29dec7971a834c22622c37f03979ad64c2
khalil1210 <khalilahmed232@gmail.com>
2017-10-16 23:22:31 +0530

    finally 100% passed

--- climbing-the-leaderboard.py
+++ climbing-the-leaderboard.py
@@ -1,31 +1,49 @@
-import sys
+def findRankOfAliceRecursive(scores, start, end, val):
+    midValue = int((start + end) / 2)
+   # print("start", start, "end", end, "val", val, "mid_i", midValue, "midScore", scores[midValue])
+
+    if val < scores[start]:
+        return start
+    elif scores[start] <= val < scores[midValue]:
+        return findRankOfAliceRecursive(scores, start, midValue, val)
+    elif scores[midValue] <= val < scores[midValue+1]:
+        return midValue+1
+    elif scores[midValue+1] <= val < scores[end]:
+        return findRankOfAliceRecursive(scores, midValue+1, end, val)
+    elif scores[end] <= val:
+        return end+1
+def findRankOfAlice(scores,aliceCurScore):
+    # 1 2 4 5
+    if ( len(scores) <= 1 ):
+        if( scores[0] > aliceCurScore ):
+            return 2
+        else:
+            return 1
+    else:
+        ans = findRankOfAliceRecursive(scores, 0, len(scores)-1, aliceCurScore)
+        return ans
 
 n = int(input().strip())
 scores = [int(scores_temp) for scores_temp in input().strip().split(' ')]
 m = int(input().strip())
 alice = [int(alice_temp) for alice_temp in input().strip().split(' ')]
 
-#removing duplicates
+# removing duplicates
 prevVal = scores[0]
 scoresNoDuplicates = [prevVal]
-for i in range(1,n):
+for i in range(1, n):
     if prevVal != scores[i]:
         scoresNoDuplicates.append(scores[i])
         prevVal = scores[i]
 
+scoresNoDuplicates.reverse()
+
+length = len(scoresNoDuplicates)
 for j in alice:
-    aliceRank = 1
-    for i in range(0,len(scoresNoDuplicates)):
 
-        if j >= scoresNoDuplicates[i]:
-            print(aliceRank)
-            break
-        else:
-            aliceRank+=1
-            if (i == len(scoresNoDuplicates)-1):
-                print(aliceRank)
+    answer = findRankOfAlice(scoresNoDuplicates,j)
+    print(length - answer + 1)
 
 
 
-#40 30 20 10
-
+# 40 30 20 10
